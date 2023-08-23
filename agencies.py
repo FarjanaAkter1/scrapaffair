f3ff6bd276c79ec505f4755085dd0df0428bd4a1
Valerie PerezPimentel <vperezpimentel@xpanxion.com>
2023-08-23 17:06:11 -0400

    get links for each company

--- agencies.py
+++ agencies.py
@@ -6,5 +6,19 @@ soup = BeautifulSoup(html.text, 'html.parser')
 
 companies = soup.findAll('a', attrs = {"data-uapi-link" : "company_name_cta"})
 
+company_list = []
+company_url_list = []
+
+
+
 for company in companies: 
-    print(company.text)
+    company_list.append(company.text.strip())
+    company_url_list.append(company.get('href'))
+
+# for link in companies_links: 
+#     print(link.text)
+# print(company_url_list)
+
+
+
+
