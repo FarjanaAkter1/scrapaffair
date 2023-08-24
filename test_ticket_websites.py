e5681e5e9be16f96261f26b6d2993b41fb34b437
Michael Reid <EfficiencyMarriesEfficacy@gmail.com>
2023-08-23 23:55:33 -0400

    Initial commit....add test_ticket_websites and ticket_websites

---
+++ test_ticket_websites.py
@@ -0,0 +1,12 @@
+import unittest
+from ticket_websites import get_company_info
+
+class TestGetCompanyInfo(unittest.TestCase):
+    def test_valid_company(self):
+        company_url = "https://www.consumeraffairs.com/entertainment/seatgeek.html"
+        company_name, company_rating = get_company_info(company_url)
+        self.assertEqual(company_name, "Example Company")
+        self.assertEqual(company_rating, "4.5")
+
+if __name__ == "__main__":
+    unittest.main()
