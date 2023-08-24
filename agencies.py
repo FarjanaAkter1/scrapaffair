ff6a0efdc72627d690ec39d8b03605837118b157
Valerie PerezPimentel <vperezpimentel@xpanxion.com>
2023-08-24 14:28:48 -0400

    start agencies info function

--- agencies.py
+++ agencies.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv 
 
 html = requests.get("https://www.consumeraffairs.com/travel/agencies.html")
 soup = BeautifulSoup(html.text, 'html.parser')
@@ -14,14 +15,28 @@ for company in companies:
     company_list.append(company.text.strip())
     company_url_list.append(company.get('href'))
 
-# for link in companies_links: 
-#     print(link.text)
-# print(company_url_list)
 
-print(company_url_list)
-html = requests.get(company_url_list[0])
-soup2 = BeautifulSoup(html.text, 'html.parser')
-reviews = soup.findAll('div', attrs ={})
+
+def agencies_info(agency_url):
+    html = requests.get(agency_url)
+    soup = BeautifulSoup(html.text, "html.parser")
+    agency_names = soup.find("span", attrs={"class" : "last-word-wrapper"})
+
+    for agency in agency_names:
+        print(agency.text)
+
+
+# print(company_url_list)
+# html2 = requests.get(company_url_list[0])
+# soup2 = BeautifulSoup(html2.text, 'html.parser')
+# reviews_div = soup2.findAll('div', attrs ={"class" : "rvw_top-text"})
+# # reviews = reviews_div.find_all('p')
+
+# print(reviews_div)
+
+agencies_info(company_url_list[0])
+
+
 
 
 
