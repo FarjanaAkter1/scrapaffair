b0d47007602c74c786403dd498d1a4a9b488ac61
Tunisia Artope <tartope@xpanxion.com>
2023-08-24 12:12:46 -0400

    Get hotel reviews for first page.

--- hotel_brands/hotel_reviews.py
+++ hotel_brands/hotel_reviews.py
@@ -2,36 +2,47 @@ from bs4 import BeautifulSoup
 import requests
 from csv import writer
 
+# HEADERS = {
+#     'User-Agent': ('Mozilla/5.0 (X11; Linux x86_64) '
+#                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/116.0 '
+#                    'AppleWebKit/537.36 (KHTML, like Gecko) '
+#                    'Chrome/44.0.2403.157 Safari/537.36'),
+#     'Accept-Language': 'en-US, en;q=0.5'
+# }
 
-response_names = requests.get("https://www.consumeraffairs.com/travel/hotels.html")
+HEADERS = {
+    'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                   'AppleWebKit/537.36 (KHTML, like Gecko) '
+                   'Chrome/115.0.0.0 Safari/537.36'),
+    'Accept-Language': 'en-US, en;q=0.9'
+}
+
+
+response_names = requests.get("https://www.consumeraffairs.com/travel/hotels.html", headers=HEADERS)
 soup_names = BeautifulSoup(response_names.text, 'html.parser')
 hotel_list = soup_names.findAll(class_="ca-a ca-a--bld-no-undln brd-card__tit-nm js-gadatalayer")
-# print(hotel_list)
-# hotel_names = []
-# hotel_names.append(hotel_list)
-# for item in hotel_list:
-#     hotel = item.text.strip()
-#     hotel_names.append(hotel)
-    # url = item["href"]
-# print(hotel_names)
+# print("h: ", hotel_list)
 
 
-def hotel_info(hotel_name):
-    response_reviews = requests.get(f"https://www.consumeraffairs.com/travel/{hotel_name}.html")
-    soup_reviews = BeautifulSoup(response_reviews.text, 'html.parser')
-    ratings_container = soup_reviews.findAll(class_="rvw__dtls")
+def hotel_info():
+    for item in hotel_list:
+        hotel = item.text.strip()
+        url = item["href"]
+        response_reviews = requests.get(url)
+        soup_reviews = BeautifulSoup(response_reviews.text, 'html.parser')
+        ratings_container = soup_reviews.findAll(class_="rvw__dtls")
 
-    result = []
+        result = []
 
-    for rating in ratings_container:
-        # Number of stars
-        star_rating = int(rating.find_all(attrs={"itemprop": "ratingValue"})[0]['content'])
-        # Review date
-        review_date = rating.find_all(class_="rvw__rvd-dt")[0].text.lstrip("Reviewed ")
-        # Reviews
-        written_reviews = rating.select("p")[0].text
+        for rating in ratings_container:
+            # Number of stars
+            star_rating = int(rating.find_all(attrs={"itemprop": "ratingValue"})[0]['content'])
+            # Review date
+            review_date = rating.find_all(class_="rvw__rvd-dt")[0].text.lstrip("Reviewed ")
+            # Reviews
+            written_reviews = rating.select("p")[0].text
 
-        result.append((star_rating, review_date, written_reviews))
+            result.append((star_rating, review_date, written_reviews))
 
     return result
 
@@ -42,7 +53,8 @@ with open("hotel_data.csv", "w") as csv_file:
 
     for item in hotel_list:
         hotel = item.text.strip()
-        reviews = hotel_info(hotel)
+        reviews = hotel_info()
 
         for star_rating, review_date, written_reviews in reviews:
             csv_writer.writerow([hotel, star_rating, review_date, written_reviews])
+
